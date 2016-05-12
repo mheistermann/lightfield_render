adf5f9f6ae5bf03ae780ed3f86facec6c9f56da2
Martin Heistermann <git@mheistermann.de>
2016-05-12 19:14:10 +0200

    texture upload in proper format

--- src/lightfield_texture.rs
+++ src/lightfield_texture.rs
@@ -8,6 +8,7 @@ extern crate lightfield_loader;
 use std::io::Read;
 use std::fs::File;
 use std::io::Cursor;
+use std::mem;
 
 use lightfield_loader::{Lightfield, LightfieldView};
 
@@ -15,6 +16,7 @@ use glium::backend::Facade;
 use glium::texture::{MipmapsOption, UncompressedUintFormat};
 use glium::texture::pixel_buffer::PixelBuffer;
 use glium::texture::unsigned_texture2d_array::UnsignedTexture2dArray;
+use std::iter::FromIterator;
 
 use image::{DynamicImage, GenericImage, ImageFormat, Pixel, Rgb};
 
@@ -35,7 +37,7 @@ impl LightfieldTexture {
         let img0 = &lf.views[0].image;
         let width = img0.width();
         let height = img0.height();
-        let buffer_size = (width * height * 3) as usize;
+        let num_pixels = (width * height) as usize;
 
         let tex = UnsignedTexture2dArray::empty_with_format(facade,
                                                             UncompressedUintFormat::U8U8U8,
@@ -60,15 +62,27 @@ impl LightfieldTexture {
             assert!(layer < n);
             // TODO: due to wrong pixelbuffer format, texture upload uses format = GL_RED_INTEGER
             // how can i write() to a pixelbuffer with T=(u8,u8,u8,u8) from a [u8] src?
-            // let buffer = PixelBuffer::<(u8, u8, u8, u8)>::new_empty(facade, buffer_size);
-            let buffer = PixelBuffer::<u8>::new_empty(facade, buffer_size);
-            buffer.write(&image.raw_pixels());
+            let buffer = PixelBuffer::<(u8, u8, u8)>::new_empty(facade, num_pixels);
+            let pixels: &Vec<u8> = &image.raw_pixels();
+
+            /* //disabled code: format-changing copy for now to make sure no bugs are here
+            // better make sure the rust tuple representation is packed,
+            // so we can kind of safely transmute
+            assert!(mem::size_of::<[(u8,u8,u8); 2]>() == 6);
+            let pixel_tuples = unsafe {mem::transmute::<&[u8], &[(u8, u8, u8)]>(pixels)};
+            buffer.write(pixel_tuples);
+            */
+            let tuples: Vec<(u8,u8,u8)> = Vec::from_iter(pixels.chunks(3).map(|v| (v[0], v[1], v[2])));
+            println!("pixels: {}", pixels.len());
+            println!("tuples: {}", tuples.len());
+            buffer.write(tuples.as_slice());
             tex.main_level().raw_upload_from_pixel_buffer(buffer.as_slice(),
                                                           0..width,
                                                           0..height,
                                                           layer..layer + 1);
             break; // XXX debug
         }
+        unsafe {tex.generate_mipmaps()};
         LightfieldTexture { tex: tex }
     }
 }
